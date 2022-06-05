00b1f7ec82ec8ab5c6a44042a29541fcde90ee8a
Jeff <j.michael.press@gmail.com>
2022-06-05 04:18:04 -0700

    complete original briefing

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # dinnerTonight
-Answering the eternal question; What's for dinner tonight?
+## Answering the eternal question; What's for dinner tonight?
+
+### Description
+This is a simple javascript program intended to run in Node. It is following the briefing from the Codecademy project titled "Mixed Messages", which I have reproduced at the end of this document.
+
+When run, the program displays a randomly generated menu consisting of an appetizer, main course and side, and a dessert, along with a *mad-libs* style summary sentence generated from adjectives related to the specific foods that were randomly chosen, to whet the user's appetite.
+
+The food/adjective pairs are stored in hard-coded arrays, and the messages are displayed to the console.
+
+<br>
+
+### Future extension ideas:
+*Read data from files instead of hard-coded arrays, which would make it easier to add more foods to expand the users options.
+*Create a front-end for a more readable and web-friendly user experience. (HTML/CSS)
+
+<hr>
+
+### Original Project Briefing
+For this project, you will build a message generator program. Every time a user runs a program, they should get a new, randomized output. You’re welcome to take the project in a couple of different forms, like an astrology generator, inspirational message, or nonsensical jokes. To make your program truly random, the message that it outputs should be made up of at least three different pieces of data. Take what you know of JavaScript syntax so far to build the program and customize it to your liking.
+
+Project Objectives:
+
+   + Build a message generator program using JavaScript
+   + Use Git version control
+   + Use command line
+   + Develop locally on your computer
+
+Prerequisites:
+
+   + JavaScript
+   + Git and GitHub
+   + Command line
--- dinnerTonight.js
+++ dinnerTonight.js
@@ -56,7 +56,46 @@ function numGen(max){
     return Math.floor(Math.random()*max);
 }
 
-console.log(randomApp());
-console.log(randomMain());
-console.log(randomSide());
-console.log(randomDessert());
+function splitArray(fullArray, index){
+    //takes in an array of arrays; returns a new array that contains the 'index' element of the internal arrays from fullArray 
+    //fullArray, 0 returns first element of each inner array in a new array
+    //fullArray, 1 returns second element of each inner array in a new array
+    //This is intended to extract the food or adjective value from the matched pairs in the arrays, but maintain the indexed relationship between the two.
+    const subArray = [];
+    for (let i = 0; i < fullArray.length; i++){
+        subArray.push(fullArray[i][index]);
+    }
+    return subArray;
+}
+
+
+function generateSummary(adjArray){
+    const sumString = `Tonight, start things off with a ${adjArray[0]} experience, followed by some ${adjArray[1]}, ${adjArray[2]} goodness, and round it all out with something a little ${adjArray[3]}.` 
+    //This function takes in an array of four Adjectives and creates a random menu madlib around them.
+    return sumString;
+}
+
+function displayMenu(foodArray, adjArray){
+    //This function takes in the two arrays and displays them appropriately
+    const summary = generateSummary(adjArray);
+    console.log(summary);
+    console.log(`Appetizer: ${foodArray[0]}.`);
+    console.log(`Main course: ${foodArray[1]} with a side of ${foodArray[2]}.`);
+    console.log(`And for dessert: ${foodArray[3]}`);
+}
+
+//Main Program
+const fullRandom = [randomApp(), randomMain(), randomSide(), randomDessert()];
+const tonightFoods = splitArray(fullRandom, 0);
+const tonightAdj = splitArray(fullRandom, 1);
+const madLib = generateSummary(tonightAdj);
+displayMenu(tonightFoods, tonightAdj);
+
+//Function Testing
+//console.log(randomApp());
+//console.log(randomMain());
+//console.log(randomSide());
+//console.log(randomDessert());
+//console.log(fullRandom);
+//console.log(splitArray(apps, 0));
+
